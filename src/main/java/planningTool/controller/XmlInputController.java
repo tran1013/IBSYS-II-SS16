75cf6981d47ad0c5dbe3bf58b2321e5954cfd378
Minh Nguyen <minh_pham@me.com>
2016-07-11 21:56:26 +0200

    fancy ui and so on

--- src/main/java/planningTool/controller/XmlInputController.java
+++ src/main/java/planningTool/controller/XmlInputController.java
@@ -2,17 +2,28 @@ package planningTool.controller;
 
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 /**
  * Created by minhnguyen on 11.07.16.
  */
-public class XmlInputController {
+public class XmlInputController{
+
+    private File f;
 
     @FXML
     private JFXTextField xmlinputpathTextField;
 
     @FXML
     public void loadXmlResultData () {
+        System.out.println("file sollte hier geoeffnet werden");
+        openFile();
+    }
 
+    public void openFile() {
+        FileChooser fileChooser = new FileChooser();
+        
     }
 }
