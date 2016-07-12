96f0e3e72daad2a026ff9e64b3337eb59a1102fb
Minh Nguyen <minh_pham@me.com>
2016-07-13 00:13:16 +0200

    console log xml import warehousestock

--- src/main/java/planningTool/controller/XmlInputController.java
+++ src/main/java/planningTool/controller/XmlInputController.java
@@ -1,15 +1,27 @@
 package planningTool.controller;
 
 import com.jfoenix.controls.JFXTextField;
+import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import planningTool.util.XmlExtractor;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by minhnguyen on 11.07.16.
  */
-public class XmlInputController{
+public class XmlInputController extends Application {
+
+    private Stage savedStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        savedStage = primaryStage;
+    }
 
     private File f;
 
@@ -19,11 +31,34 @@ public class XmlInputController{
     @FXML
     public void loadXmlResultData () {
         System.out.println("file sollte hier geoeffnet werden");
-        openFile();
+        initXmlImport();
     }
 
-    public void openFile() {
+    public void initXmlImport() {
         FileChooser fileChooser = new FileChooser();
-        
+        fileChooser.setTitle("Open XML file");
+
+        FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("XML Files(*.xml)", "*.xml");
+        fileChooser.getExtensionFilters().addAll(filter);
+
+        File selectedFile = fileChooser.showOpenDialog(savedStage);
+
+        if (selectedFile != null) {
+            String text = selectedFile.getPath();
+            String text2 = selectedFile.getName();
+
+            xmlinputpathTextField.setText(text2);
+
+            System.out.println("File \n" + selectedFile);
+
+            try {
+                new XmlExtractor().parseXML(selectedFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ParserConfigurationException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
