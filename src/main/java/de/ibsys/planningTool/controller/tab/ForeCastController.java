926e91618a97cfbc33e8914e28d500cf6188cdd4
Minh Nguyen <minh_pham@me.com>
2016-07-18 15:40:40 +0200

    own TextField

--- src/main/java/de/ibsys/planningTool/controller/tab/ForeCastController.java
+++ src/main/java/de/ibsys/planningTool/controller/tab/ForeCastController.java
@@ -2,7 +2,7 @@ package de.ibsys.planningTool.controller.tab;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
-import de.ibsys.planningTool.controller.MainController;
+import de.ibsys.planningTool.util.JFXIntegerTextField;
 import javafx.fxml.FXML;
 import javafx.stage.Stage;
 
@@ -14,17 +14,64 @@ public class ForeCastController extends BaseTabController{
     public JFXButton saveForeCastViewButton;
 
     @FXML
-    private JFXTextField forecast1TextField;
+    private JFXIntegerTextField forecast1TextField;
+    @FXML
+    private JFXIntegerTextField forecast2TextField;
+    @FXML
+    private JFXIntegerTextField forecast3TextField;
+    @FXML
+    private JFXIntegerTextField p1nTextField;
+    @FXML
+    private JFXIntegerTextField p1n1TextField;
+    @FXML
+    private JFXIntegerTextField p1n2TextField;
+    @FXML
+    private JFXIntegerTextField p1n3TextField;
+    @FXML
+    private JFXIntegerTextField p2nTextField;
+    @FXML
+    private JFXIntegerTextField p2n1TextField;
+    @FXML
+    private JFXIntegerTextField p2n2TextField;
+    @FXML
+    private JFXIntegerTextField p2n3TextField;
+    @FXML
+    private JFXIntegerTextField p3nTextField;
+    @FXML
+    private JFXIntegerTextField p3n1TextField;
+    @FXML
+    private JFXIntegerTextField p3n2TextField;
+    @FXML
+    private JFXIntegerTextField p3n3TextField;
 
     @FXML
-    private JFXTextField forecast2TextField;
-
+    private JFXIntegerTextField directSalesPriceP1TextField;
     @FXML
-    private JFXTextField forecast3TextField;
+    private JFXIntegerTextField directSalesPriceP2TextField;
+    @FXML
+    private JFXIntegerTextField directSalesPriceP3TextField;
+    @FXML
+    private JFXIntegerTextField directSalesAmountP1TextField;
+    @FXML
+    private JFXIntegerTextField directSalesAmountP2TextField;
+    @FXML
+    private JFXIntegerTextField directSalesAmountP3TextField;
+    @FXML
+    private JFXTextField directSalesPunishmentP1TextField;
+    @FXML
+    private JFXTextField directSalesPunishmentP2TextField;
+    @FXML
+    private JFXTextField directSalesPunishmentP3TextField;
 
     @FXML
     private void saveForeCastInformation() {
     }
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
---
+++ src/main/java/de/ibsys/planningTool/util/JFXIntegerTextField.java
@@ -0,0 +1,27 @@
+package de.ibsys.planningTool.util;
+
+import com.jfoenix.controls.JFXTextField;
+
+/**
+ * Created by minhnguyen on 18.07.16.
+ */
+public class JFXIntegerTextField extends JFXTextField {
+
+    @Override
+    public void replaceText(int start, int end, String text) {
+        if (validate(text)) {
+            super.replaceText(start, end, text);
+        }
+    }
+
+    @Override
+    public void replaceSelection(String text) {
+        if (validate(text)) {
+            super.replaceSelection(text);
+        }
+    }
+
+    private boolean validate(String text) {
+        return text.matches("[0-9]*");
+    }
+}
