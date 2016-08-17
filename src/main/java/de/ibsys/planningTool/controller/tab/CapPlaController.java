1041f364ab73f25efe90b13d15025940b3f280e5
Anh Duc Tran <anhduc.t92@gmail.com>
2016-08-17 19:37:58 +0200

    separate controller and service class. clean some mess up.

--- src/main/java/de/ibsys/planningTool/controller/tab/CapPlaController.java
+++ src/main/java/de/ibsys/planningTool/controller/tab/CapPlaController.java
@@ -10,6 +10,7 @@ import de.ibsys.planningTool.model.XmlInputData;
 import de.ibsys.planningTool.model.xmlInputModel.OrdersInWork;
 import de.ibsys.planningTool.model.xmlInputModel.WaitingList;
 import de.ibsys.planningTool.model.xmlInputModel.WaitingListWorkPlace;
+import de.ibsys.planningTool.service.CapPla;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -48,281 +49,27 @@ public class CapPlaController extends BaseTabController {
     private TableColumn<CapPlaResult, Integer> overtimeCol;
 
     @FXML
-    private TableColumn infoCol;
-
-    @FXML
-    private NumberAxis yAxis;
-
-    @FXML
-    private CategoryAxis xAxis;
-
-    @FXML
     private BarChart<Number, String> barChart;
-
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
     }
 
-    XmlInputData input;
-    XmlInputController inputCon = new XmlInputController();
-    capPlaDB prod = new capPlaDB();
-
-    /**
-     * God Method to calculate CapPla shit
-     * TODO: return Object from CapPlaResult
-     */
-    @FXML
-    public List<CapPlaResult> calculateCap() {
-
-        List<Integer> workplaceIDs;
-        Map<String, Integer> capResult = new HashMap<>();
-        Map<String, Integer> setupTimeList = new HashMap<>();
-        Map<String, Integer> ordersInWorkTime = new HashMap<>();
-        Integer setupTimeLastPeriod = 0;
-
-        List<CapPlaResult> resultCapPla = new ArrayList<>();
-
-        //TODO: Replace mock datas with real from dispo (1/2)
-        sellData mock = new sellData();
-
-
-        try {
-            workplaceIDs = prod.findWorkplaceID();
-
-            //TODO: Replace mock datas with real from dispo (2/3)
-            mock.setProdSteps();
-            List<ProductionSteps> prodSteps = mock.getProdSteps();
-
-            System.out.println();
-            System.out.println("Start CAPLA BOSS METHOD!!!");
-            System.out.println();
-
-            for (Integer i : workplaceIDs) {
-                //workplaceNumber begins by 1
-                Integer capacity = 0;
-                Integer CapSetupTime = 0;
-                Integer workplace = 0;
-                Integer result = 0;
-                Integer productionTime = 0;
-                Integer setup = 0;
-                Integer requirePeriod = 0;
-                Integer shifts = 0;
-                Integer overtime = 0;
-
-                for (ProductionSteps ps : prod.findByWorkplacID(i)) {
-
-                    workplace = ps.getWorkplaceID();
-                    String Item = ps.getItemConfigID();
-                    productionTime = ps.getProductionTime();
-                    setup = ps.getSetupTime();
-
-                    //   System.out.println("Workplace: " + workplace + " - Item: " + Item + " - ProductionTime: " + productionTime);
-
-                    //TODO: Replace mock datas with real from dispo (3/3)
-                    for (ProductionSteps cap : prodSteps) {
-
-                        String item = cap.getItemConfigID();
-
-                        if (item.equals(Item) && cap.getProductionTime() >= 0) {
-                            Integer needTime = cap.getProductionTime();
-
-                            CapSetupTime += setup;
-                            capacity += (productionTime * needTime);
-                            //    System.out.println("Needtime: " + needTime);
-                            //   System.out.println("ItemID: " + item);
-                        }
-                    }
-                    /*   System.out.println("Workplace: " + workplace + " - Item: " + Item + " - ProdutionTime: " + productionTime);
-                     System.out.println("Workplace: " + workplace + " - Capacity: " + capacity + " - SetupTime: " + CapSetupTime);*/
-
-                    capResult.put(workplace.toString(), capacity);
-                    setupTimeList.put(workplace.toString(), CapSetupTime);
-                }
-
-                // System.out.println("Finish Workplace: " + i);
-                System.out.println("Workplace: " + workplace);
-                System.out.println("Cap. require: " + capResult.get(workplace.toString()));
-                System.out.println("Setup Time: " + setupTimeList.get(workplace.toString()));
-
-                result = this.getAllScheduledTime(workplace);
-                result = result * productionTime;
-                System.out.println("Cap. require last period: " + result);
-                ordersInWorkTime.put(workplace.toString(), result);
-
-                setupTimeLastPeriod = this.getPieceAmountOrdersInWork(workplace);
-                setupTimeLastPeriod = setupTimeLastPeriod * setup;
-                System.out.println("SetupTime last period: " + setupTimeLastPeriod);
-                System.out.println("_________________________");
-
-                requirePeriod = capResult.get(workplace.toString()) + setupTimeList.get(workplace.toString()) + result + setupTimeLastPeriod;
-                shifts = this.calculateShifts(requirePeriod);
-                overtime = this.calculateOvertime(requirePeriod, shifts);
-
-                resultCapPla.add(new CapPlaResult(workplace, requirePeriod, shifts, overtime));
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-
-        }
-        /*System.out.println("SETUPTIME: ");
-        System.out.println(Arrays.toString(setupTimeList.entrySet().toArray()));*/
-
-
-        return resultCapPla;
-    }
-
-    /**
-     * Get the amount of article per workplace in OrdersInWork XML
-     *
-     * @param workplace
-     * @return
-     */
-    @FXML
-    public Integer getOrdersinWorkAmount(Integer workplace) {
-
-        Integer workplaceID = null;
-        Integer amount = 0;
-        Integer piece = 0;
-
-        Map<String, OrdersInWork> ordersInWorkMap;
-        ordersInWorkMap = main.getXmlInputData().getOrdersInWorkMap();
-        Map<String, OrdersInWork> order = new TreeMap<>(ordersInWorkMap);
-
-        // System.out.println("OrderInWork:");
-        for (Map.Entry<String, OrdersInWork> orderMap : order.entrySet()) {
-            //Check workplaceID to get amount
-            if (orderMap.getValue().getId().equals(workplace.toString())) {
-                piece++;
-                workplaceID = Integer.parseInt(orderMap.getValue().getId());
-                String articleID = orderMap.getValue().getArticleId();
-                amount = orderMap.getValue().getAmount();
-                //   System.out.println("WorkplaceID: " + workplaceID + " ArticleID: " + articleID + " Amount: " + amount);
-            }
-        }
-
-
-
-        /*System.out.println("Workplace: " + workplace);
-        System.out.println("Piece OrdersInWork: " + piece + " " + "Amount: " + amount);*/
-        return amount;
-    }
-
-    /**
-     * Get the amount of article per workplace in WaitingListWorkstations XML
-     *
-     * @param workplace
-     * @return
-     */
-    public Integer getWaitinglistAmount(Integer workplace) {
-
-        Integer workplaceID = null;
-        Integer amount = 0;
-
-        Map<String, WaitingListWorkPlace> waitingListWorkPlaceMap;
-        waitingListWorkPlaceMap = main.getXmlInputData().getWaitingListWorkPlaceMap();
-        Map<String, WaitingListWorkPlace> wait = new TreeMap<>(waitingListWorkPlaceMap);
-
-        //  System.out.println("WaitingLists");
-        for (WaitingListWorkPlace waitPlace : wait.values()) {
-            //Check workplaceID to get amount
-            if (waitPlace.getWorkplaceId().equals(workplace.toString()) && waitPlace.getNecessaryTime() > 0) {
-                workplaceID = Integer.parseInt(waitPlace.getWorkplaceId());
-                for (WaitingList list : waitPlace.getWaitingLists()) {
-                    String articleID = list.getArticleId();
-                    amount = list.getAmount();
-                    //    System.out.println("workplaceID: " + workplaceID + " articleID: " + articleID + " Amount: " + amount);
-                }
-            }
-
-        }
-
-        /*System.out.println("Piece WaitingList: " + piece + " Amount: " + amount);
-        System.out.println("__________________________");
-        System.out.println();*/
-        return amount;
-    }
-
-    /**
-     * Get the numbers of pieces from ordersInWork to calculate scheduled setup time from last period
-     *
-     * @param workplace
-     * @return
-     */
-    public Integer getPieceAmountOrdersInWork(Integer workplace) {
-        Integer piece = 0;
-        Integer workplaceID = null;
-        Map<String, OrdersInWork> ordersInWorkMap;
-        ordersInWorkMap = main.getXmlInputData().getOrdersInWorkMap();
-
-        Map<String, OrdersInWork> order = new TreeMap<>(ordersInWorkMap);
-
-        //System.out.println("Amount OrdersInWorK:");
-        for (Map.Entry<String, OrdersInWork> orderMap : order.entrySet()) {
-            //Check workplaceID to get amount
-            if (orderMap.getValue().getId().equals(workplace.toString())) {
-                piece++;
-            }
-        }
-        //System.out.println("Workplace: " + workplace + " - Piece: " + piece);
-        return piece;
-    }
-
-    /**
-     * !!!DON'T DELETE THIS METHOD!!!
-     * Not clear whether this method is important, because it's enough to get the amount from getPieceAmountOrdersInWork()
-     * Get the numbers of pieces from WaitingListWorkstations XML to calculate scheduled setup time from last period
-     * @param workplace
-     * @return
-     */
-/*    public Integer getPieceAmountWaitingList(Integer workplace){
-        Integer piece = 0;
-
-        Map<String, WaitingListWorkPlace> waitingListWorkPlaceMap;
-        waitingListWorkPlaceMap = main.getXmlInputData().getWaitingListWorkPlaceMap();
-        Map<String, WaitingListWorkPlace> wait = new TreeMap<>(waitingListWorkPlaceMap);
-
-        //System.out.println("Amount WaitingList");
-        for (WaitingListWorkPlace waitPlace : wait.values()) {
-            //Check workplaceID to get amount
-            if (waitPlace.getWorkplaceId().equals(workplace.toString()) && waitPlace.getNecessaryTime() > 0) {
-                for (WaitingList list : waitPlace.getWaitingLists()) {
-                    piece++;
-                }
-            }
-
-        }
-        System.out.println("Workplace: " + workplace + " - Piece: " + piece);
-        return piece;
-    }*/
-
-    /**
-     * Get all needed times back to calculate capacity requirement of last period
-     *
-     * @param workplace
-     * @return
-     */
-    public Integer getAllScheduledTime(Integer workplace) {
-        Integer result = null;
-        Integer scheduledOrdersInWork = this.getOrdersinWorkAmount(workplace);
-        Integer waitinglistAmount = this.getWaitinglistAmount(workplace);
-
-        result = scheduledOrdersInWork + waitinglistAmount;
-
-        return result;
-    }
 
     /**
      * Start get CapPla results from here
      */
     @FXML
     public List<CapPlaResult> getMasterResult() {
-
+        CapPla cap = new CapPla();
         List<CapPlaResult> result;
+        Map<String, OrdersInWork> ordersInWorkMap;
+        Map<String, WaitingListWorkPlace> waitingListWorkPlaceMap;
 
-        result = this.calculateCap();
+        ordersInWorkMap = main.getXmlInputData().getOrdersInWorkMap();
+        waitingListWorkPlaceMap = main.getXmlInputData().getWaitingListWorkPlaceMap();
+        result = cap.calculateCap(ordersInWorkMap, waitingListWorkPlaceMap);
 
         System.out.println("Print list");
         System.out.println(result);
@@ -330,48 +77,6 @@ public class CapPlaController extends BaseTabController {
         return result;
     }
 
-    /**
-     * Calculate shifts per workplace
-     *
-     * @param requirePeriod
-     * @return
-     */
-    public Integer calculateShifts(Integer requirePeriod) {
-
-        if (requirePeriod <= 0) {
-            return 0;
-        }
-        if (requirePeriod >= 1 && requirePeriod <= 3600) {
-            return 1;
-        }
-        if (requirePeriod >= 3601 && requirePeriod <= 6000) {
-            return 2;
-        }
-        if (requirePeriod >= 6001) {
-            return 3;
-        }
-
-        return -1;
-    }
-
-    /**
-     * Calculate overtime per workplace
-     *
-     * @param reqCapacity
-     * @param shifts
-     * @return
-     */
-    protected int calculateOvertime(int reqCapacity, int shifts) {
-        if (shifts == 3) {
-            return 0;
-        }
-
-        int overtime = reqCapacity - (shifts * Constants.WORKPLACE_CAPACITY);
-        if (overtime <= 0) {
-            return 0;
-        }
-        return overtime;
-    }
 
     /**
      * Get ObservableList with datas to print them on the table view
@@ -410,14 +115,11 @@ public class CapPlaController extends BaseTabController {
 
         Integer workplaceId;
         Integer reqCapacity;
-        Integer shifts;
-        Integer overtime;
+
 
         for (CapPlaResult cap : capPlaList) {
             workplaceId = cap.getWorkplaceId();
             reqCapacity = cap.getReqCapacity();
-            shifts = cap.getShifts();
-            overtime = cap.getOvertime();
 
             dataSet.getData().add(new XYChart.Data(workplaceId.toString(), reqCapacity));
         }
@@ -432,17 +134,14 @@ public class CapPlaController extends BaseTabController {
      */
     @FXML
     private void getCapPlaUI() {
+        XYChart.Series dataSet = new XYChart.Series();
 
         workplaceCol.setCellValueFactory(new PropertyValueFactory<>("workplaceId"));
         capacityCol.setCellValueFactory(new PropertyValueFactory<>("reqCapacity"));
         shiftsCol.setCellValueFactory(new PropertyValueFactory<>("shifts"));
         overtimeCol.setCellValueFactory(new PropertyValueFactory<>("overtime"));
 
-
         tableView.setItems(getResults());
-
-        XYChart.Series dataSet = new XYChart.Series();
-
         barChart.getData().add(getBarChartData());
     }
 
--- src/main/java/de/ibsys/planningTool/service/CapPla.java
+++ src/main/java/de/ibsys/planningTool/service/CapPla.java
@@ -1,70 +1,80 @@
 package de.ibsys.planningTool.service;
 
-
-//import com.sun.tools.corba.se.idl.InterfaceGen;
-import de.ibsys.planningTool.mock.sellData;
-import de.ibsys.planningTool.model.ProductionSteps;
+import de.ibsys.planningTool.controller.tab.XmlInputController;
 import de.ibsys.planningTool.database.capPlaDB;
+import de.ibsys.planningTool.mock.sellData;
+import de.ibsys.planningTool.model.CapPlaResult;
+import de.ibsys.planningTool.model.Constants;
+import de.ibsys.planningTool.model.ProductionSteps;
 import de.ibsys.planningTool.model.XmlInputData;
 import de.ibsys.planningTool.model.xmlInputModel.OrdersInWork;
-import org.jdom2.Element;
-import org.springframework.beans.factory.annotation.Autowired;
+import de.ibsys.planningTool.model.xmlInputModel.WaitingList;
+import de.ibsys.planningTool.model.xmlInputModel.WaitingListWorkPlace;
+import javafx.fxml.FXML;
 
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
- * Created by Duc on 11.07.16.
+ * TODO: Clean up mess in class
+ * TODO: Test class or take a deep breath and get to it?
+ * Created by Duc on 17.08.16.
  */
 public class CapPla {
 
-
+    XmlInputData input;
+    XmlInputController inputCon = new XmlInputController();
     capPlaDB prod = new capPlaDB();
 
-    @Autowired
-    XmlInputData input = new XmlInputData();
-
-
     /**
-     * Method to get CapReq
-     * TODO: delete unused println
-     * @return
+     * God Method to calculate CapPla shit
+     * TODO: return Object from CapPlaResult
      */
-    public Map<String, Integer> getCapResult() {
+    @FXML
+    public List<CapPlaResult> calculateCap(Map<String, OrdersInWork> ordersInWorkMap, Map<String, WaitingListWorkPlace> waitingListWorkPlaceMap) {
 
         List<Integer> workplaceIDs;
-        sellData mock = new sellData();
         Map<String, Integer> capResult = new HashMap<>();
         Map<String, Integer> setupTimeList = new HashMap<>();
+        Map<String, Integer> ordersInWorkTime = new HashMap<>();
+        Integer setupTimeLastPeriod = 0;
+
+        List<CapPlaResult> resultCapPla = new ArrayList<>();
+
+        //TODO: Replace mock datas with real from dispo (1/2)
+        sellData mock = new sellData();
 
 
         try {
             workplaceIDs = prod.findWorkplaceID();
+
+            //TODO: Replace mock datas with real from dispo (2/3)
             mock.setProdSteps();
             List<ProductionSteps> prodSteps = mock.getProdSteps();
 
+            System.out.println();
+            System.out.println("Start CAPLA BOSS METHOD!!!");
+            System.out.println();
 
-            System.out.println("GO!");
-
-            for (int i : workplaceIDs) {
-                //workplaceNumber begins by 1
-                int capacity = 0;
-                int setupTime = 0;
-                Integer workplace;
+            for (Integer i : workplaceIDs) {
+                Integer capacity = 0;
+                Integer CapSetupTime = 0;
+                Integer workplace = 0;
+                Integer result;
+                Integer productionTime = 0;
+                Integer setup = 0;
+                Integer requirePeriod;
+                Integer shifts;
+                Integer overtime;
 
                 for (ProductionSteps ps : prod.findByWorkplacID(i)) {
 
                     workplace = ps.getWorkplaceID();
                     String Item = ps.getItemConfigID();
-                    Integer productionTime = ps.getProductionTime();
-                    Integer setup = ps.getSetupTime();
+                    productionTime = ps.getProductionTime();
+                    setup = ps.getSetupTime();
 
-                    //   System.out.println("Workplace: " + workplace + " - Item: " + Item + " - ProductionTime: " + productionTime);
-
-                    //TODO: Replace mock datas with real from dispo
+                    //TODO: Replace mock datas with real from dispo (3/3)
                     for (ProductionSteps cap : prodSteps) {
 
                         String item = cap.getItemConfigID();
@@ -72,46 +82,189 @@ public class CapPla {
                         if (item.equals(Item) && cap.getProductionTime() >= 0) {
                             Integer needTime = cap.getProductionTime();
 
-                            setupTime += setup;
+                            CapSetupTime += setup;
                             capacity += (productionTime * needTime);
-                            //    System.out.println("Needtime: " + needTime);
                         }
-
                     }
-                    //  System.out.println("Workplace: " + workplace + " - Item: " + Item + " - ProdutionTime: " + productionTime);
-                    // System.out.println("Workplace: " + workplace + " - Capacity: " + capacity + " - SetupTime: " + setupTime);
                     capResult.put(workplace.toString(), capacity);
-                    setupTimeList.put(workplace.toString(), setupTime);
+                    setupTimeList.put(workplace.toString(), CapSetupTime);
                 }
+                System.out.println("Workplace: " + workplace);
+                System.out.println("Cap. require: " + capResult.get(workplace.toString()));
+                System.out.println("Setup Time: " + setupTimeList.get(workplace.toString()));
 
-                // System.out.println("Finish Workplace: " + i);
+                result = this.getAllScheduledTime(workplace, ordersInWorkMap, waitingListWorkPlaceMap);
+                result = result * productionTime;
+                System.out.println("Cap. require last period: " + result);
+                ordersInWorkTime.put(workplace.toString(), result);
 
+                setupTimeLastPeriod = this.getPieceAmountOrdersInWork(workplace, ordersInWorkMap);
+                setupTimeLastPeriod = setupTimeLastPeriod * setup;
+                System.out.println("SetupTime last period: " + setupTimeLastPeriod);
+                System.out.println("_________________________");
+
+                requirePeriod = capResult.get(workplace.toString()) + setupTimeList.get(workplace.toString()) + result + setupTimeLastPeriod;
+                shifts = this.calculateShifts(requirePeriod);
+                overtime = this.calculateOvertime(requirePeriod, shifts);
+
+                resultCapPla.add(new CapPlaResult(workplace, requirePeriod, shifts, overtime));
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
 
         }
-      //  System.out.println("SETUPTIME: ");
-      //  System.out.println(Arrays.toString(setupTimeList.entrySet().toArray()));
-        return capResult;
+
+        return resultCapPla;
     }
 
+    /**
+     * Get the amount of article per workplace in OrdersInWork XML
+     *
+     * @param workplace
+     * @return
+     */
+    @FXML
+    public Integer getOrdersinWorkAmount(Integer workplace, Map<String, OrdersInWork> ordersInWorkMap) {
 
-    public void getXML(){
+        Integer amount = 0;
+        Map<String, OrdersInWork> order = new TreeMap<>(ordersInWorkMap);
 
+        for (Map.Entry<String, OrdersInWork> orderMap : order.entrySet()) {
+            if (orderMap.getValue().getId().equals(workplace.toString())) {
+                amount = orderMap.getValue().getAmount();
+            }
+        }
 
-        Map<String, OrdersInWork> myList = new HashMap<>();
-
-        myList = input.getOrdersInWorkMap();
-
-        System.out.println("GET XML");
-        System.out.println(myList);
-
+        return amount;
     }
 
+    /**
+     * Get the amount of article per workplace in WaitingListWorkstations XML
+     *
+     * @param workplace
+     * @return
+     */
+    public Integer getWaitinglistAmount(Integer workplace, Map<String, WaitingListWorkPlace> waitingListWorkPlaceMap) {
 
+        Integer amount = 0;
+        Map<String, WaitingListWorkPlace> wait = new TreeMap<>(waitingListWorkPlaceMap);
 
+        for (WaitingListWorkPlace waitPlace : wait.values()) {
+            if (waitPlace.getWorkplaceId().equals(workplace.toString()) && waitPlace.getNecessaryTime() > 0) {
+                for (WaitingList list : waitPlace.getWaitingLists()) {
+                    amount = list.getAmount();
+                }
+            }
+        }
+
+        return amount;
+    }
+
+    /**
+     * Get the numbers of pieces from ordersInWork to calculate scheduled setup time from last period
+     *
+     * @param workplace
+     * @return
+     */
+    public Integer getPieceAmountOrdersInWork(Integer workplace, Map<String, OrdersInWork> ordersInWorkMap) {
+
+        Integer piece = 0;
+        Map<String, OrdersInWork> order = new TreeMap<>(ordersInWorkMap);
+
+        for (Map.Entry<String, OrdersInWork> orderMap : order.entrySet()) {
+            if (orderMap.getValue().getId().equals(workplace.toString())) {
+                piece++;
+            }
+        }
+
+        return piece;
+    }
+
+    /**
+     * !!!DON'T DELETE THIS METHOD!!!
+     * Not clear whether this method is important, because it's enough to get the amount from getPieceAmountOrdersInWork()
+     * Get the numbers of pieces from WaitingListWorkstations XML to calculate scheduled setup time from last period
+     * @param workplace
+     * @return
+     */
+/*    public Integer getPieceAmountWaitingList(Integer workplace){
+        Integer piece = 0;
+
+        Map<String, WaitingListWorkPlace> waitingListWorkPlaceMap;
+        waitingListWorkPlaceMap = main.getXmlInputData().getWaitingListWorkPlaceMap();
+        Map<String, WaitingListWorkPlace> wait = new TreeMap<>(waitingListWorkPlaceMap);
+
+        //System.out.println("Amount WaitingList");
+        for (WaitingListWorkPlace waitPlace : wait.values()) {
+            //Check workplaceID to get amount
+            if (waitPlace.getWorkplaceId().equals(workplace.toString()) && waitPlace.getNecessaryTime() > 0) {
+                for (WaitingList list : waitPlace.getWaitingLists()) {
+                    piece++;
+                }
+            }
+
+        }
+        System.out.println("Workplace: " + workplace + " - Piece: " + piece);
+        return piece;
+    }*/
+
+    /**
+     * Get all needed times back to calculate capacity requirement of last period
+     *
+     * @param workplace
+     * @return
+     */
+    public Integer getAllScheduledTime(Integer workplace, Map<String, OrdersInWork> ordersInWorkMap, Map<String, WaitingListWorkPlace> waitingListWorkPlaceMap) {
+        Integer result = null;
+        Integer scheduledOrdersInWork = this.getOrdersinWorkAmount(workplace, ordersInWorkMap);
+        Integer waitinglistAmount = this.getWaitinglistAmount(workplace, waitingListWorkPlaceMap);
+
+        result = scheduledOrdersInWork + waitinglistAmount;
+
+        return result;
+    }
+
+    /**
+     * Calculate shifts per workplace
+     *
+     * @param requirePeriod
+     * @return
+     */
+    public Integer calculateShifts(Integer requirePeriod) {
+
+        if (requirePeriod <= 0) {
+            return 0;
+        }
+        if (requirePeriod >= 1 && requirePeriod <= 3600) {
+            return 1;
+        }
+        if (requirePeriod >= 3601 && requirePeriod <= 6000) {
+            return 2;
+        }
+        if (requirePeriod >= 6001) {
+            return 3;
+        }
+
+        return -1;
+    }
+
+    /**
+     * Calculate overtime per workplace
+     *
+     * @param reqCapacity
+     * @param shifts
+     * @return
+     */
+    protected int calculateOvertime(int reqCapacity, int shifts) {
+        if (shifts == 3) {
+            return 0;
+        }
+
+        int overtime = reqCapacity - (shifts * Constants.WORKPLACE_CAPACITY);
+        if (overtime <= 0) {
+            return 0;
+        }
+        return overtime;
+    }
 }
-
-
