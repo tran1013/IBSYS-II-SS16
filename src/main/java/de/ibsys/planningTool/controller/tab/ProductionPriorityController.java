c1494e7510cf021339c0aebae0407f6b664e4b35
Minh Nguyen <minh_pham@me.com>
2016-08-27 16:04:21 +0200

    drag and drop with mock data

--- src/main/java/de/ibsys/planningTool/controller/tab/ProductionPriorityController.java
+++ src/main/java/de/ibsys/planningTool/controller/tab/ProductionPriorityController.java
@@ -5,21 +5,35 @@ import de.ibsys.planningTool.controller.MainController;
 import de.ibsys.planningTool.model.xmlExportModel.Item;
 import de.ibsys.planningTool.util.MockObject;
 import de.ibsys.planningTool.util.PriorityCell;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.stage.Stage;
-import javafx.util.Callback;
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
+ * TODO Refactor all this cell stuff in the cell class
  * Created by minhnguyen on 26.08.16.
  */
 public class ProductionPriorityController extends BaseTabController {
 
+    Logger logger = Logger.getLogger(ProductionPriorityController.class.getSimpleName());
+
     @FXML
     private JFXListView listView;
+
+    //TODO REPLACE THIS SHIT
+    private List<Item> mockItems = new MockObject().productionListMockData();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -28,11 +42,88 @@ public class ProductionPriorityController extends BaseTabController {
     @Override
     public void init(MainController main) {
         super.init(main);
-        ObservableList<Item> productionList = FXCollections.observableList(new MockObject().productionListMockData());
 
+        logger.info(mockItems.toString());
+
+        final IntegerProperty dragFromIndex = new SimpleIntegerProperty(-1);
+        ObservableList<Item> productionList = FXCollections.observableArrayList(mockItems);
         listView.setItems(productionList);
-        listView.setCellFactory(itemListView -> new PriorityCell());
+        listView.setCellFactory(itemListView -> {
+            PriorityCell cell = new PriorityCell(productionList);
 
-        System.out.println(System.getProperties());
+            cell.setOnDragDetected(event -> {
+                if (!cell.isEmpty()) {
+                    dragFromIndex.set(cell.getIndex());
+                    Dragboard db = cell.startDragAndDrop(TransferMode.MOVE);
+                    ClipboardContent cc = new ClipboardContent();
+                    cc.putString(String.valueOf(cell.getItem()));
+                    db.setContent(cc);
+                    db.setDragView(cell.snapshot(null, null));
+                }
+            });
+
+            cell.setOnDragOver(event -> {
+                if (dragFromIndex.get() >= 0 && dragFromIndex.get() != cell.getIndex()) {
+                    event.acceptTransferModes(TransferMode.MOVE);
+                }
+            });
+
+            cell.setOnDragEntered(event -> {
+                if (dragFromIndex.get() >= 0 && dragFromIndex.get() != cell.getIndex()) {
+                    cell.setOpacity(0.3);
+                }
+            });
+
+            cell.setOnDragExited(event -> {
+                cell.setOpacity(1);
+            });
+
+            cell.setOnDragDropped(event -> {
+
+                int dragItemsStartIndex;
+                int dragItemsEndIndex;
+                int direction;
+                if (cell.isEmpty()) {
+                    dragItemsStartIndex = dragFromIndex.get();
+                    dragItemsEndIndex = listView.getItems().size();
+                    direction = -1;
+                } else {
+                    if (cell.getIndex() < dragFromIndex.get()) {
+                        dragItemsStartIndex = cell.getIndex();
+                        dragItemsEndIndex = dragFromIndex.get() + 1;
+                        direction = 1;
+                    } else {
+                        dragItemsStartIndex = dragFromIndex.get();
+                        dragItemsEndIndex = cell.getIndex() + 1;
+                        direction = -1;
+                    }
+                }
+
+                List<Item> rotatingItems = listView.getItems().subList(dragItemsStartIndex, dragItemsEndIndex);
+                logger.info(rotatingItems);
+
+                List<Item> rotatingItemsCopy = new ArrayList<>(rotatingItems);
+                logger.info(rotatingItemsCopy);
+
+                Collections.rotate(rotatingItemsCopy, direction);
+                rotatingItems.clear();
+                rotatingItems.addAll(rotatingItemsCopy);
+
+                mockItems.clear();
+
+                Arrays.stream(listView.getItems().toArray()).forEach(item -> {
+                    mockItems.add((Item) item);
+                });
+
+                dragFromIndex.set(-1);
+            });
+
+            cell.setOnDragDone(event -> {
+                dragFromIndex.set(-1);
+                listView.getSelectionModel().select(event.getDragboard().getString());
+            });
+
+            return cell;
+        });
     }
 }
--- src/main/java/de/ibsys/planningTool/util/PriorityCell.java
+++ src/main/java/de/ibsys/planningTool/util/PriorityCell.java
@@ -2,10 +2,16 @@ package de.ibsys.planningTool.util;
 
 import com.jfoenix.controls.JFXListCell;
 import de.ibsys.planningTool.model.xmlExportModel.Item;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import org.apache.log4j.Logger;
 
 import java.io.IOException;
 
@@ -13,7 +19,10 @@ import java.io.IOException;
  * custom list view for priorities
  * Created by minhnguyen on 26.08.16.
  */
-public class PriorityCell extends JFXListCell<Item> {
+public class PriorityCell extends JFXListCell<Item> implements ChangeListener<Number> {
+
+    private Logger logger = Logger.getLogger(PriorityCell.class.getSimpleName());
+
     @FXML
     private Label item_id;
 
@@ -23,7 +32,114 @@ public class PriorityCell extends JFXListCell<Item> {
     @FXML
     private GridPane gridPane_priority;
 
+    private ObservableList<Item> items;
+
     private FXMLLoader mLLoader;
+
+    private IntegerProperty dragFromIndex = new SimpleIntegerProperty(-1);
+
+
+    public PriorityCell(ObservableList<Item> items) {
+
+        this.items = items;
+
+//        JFXListCell cell = this;
+//
+//        setOnDragDetected(event -> {
+//            if (getItem().equals(null)) {
+//                return;
+//            }
+//            dragFromIndex.set(cell.getIndex());
+//
+//            Dragboard dragboard = cell.startDragAndDrop(TransferMode.MOVE);
+//
+//            ClipboardContent clipboardContent = new ClipboardContent();
+//            clipboardContent.putString(String.valueOf(cell.getItem()));
+//
+//            dragboard.setContent(clipboardContent);
+//
+//            dragboard.setDragView(cell.snapshot(null, null));
+//            event.consume();
+//        });
+//
+//        setOnDragOver(event -> {
+//            if (event.getGestureSource() != cell &&
+//                    event.getDragboard().hasString()) {
+//                event.acceptTransferModes(TransferMode.MOVE);
+//            }
+//            event.consume();
+//        });
+//
+//        setOnDragEntered(event -> {
+//            if (event.getGestureSource() != cell &&
+//                    event.getDragboard().hasString()) {
+//                setOpacity(0.3);
+//            }
+//        });
+//
+//        setOnDragExited(event -> {
+//            if (event.getGestureSource() != cell &&
+//                    event.getDragboard().hasString()) {
+//                setOpacity(1);
+//            }
+//        });
+//
+//        setOnDragDropped(event -> {
+//
+//            if (getItem() == null) {
+//                return;
+//            }
+//
+//            Dragboard dragboard = event.getDragboard();
+//
+//            boolean success = false;
+//            if (dragboard.hasString()) {
+//                ObservableList<Item> tmpList = getListView().getItems();
+////
+////                String tmpDraggedIndex = dragboard.getString();
+////                int thisIndex = tmpList.indexOf(getItem());
+////
+////                logger.info("Dragged Index = " + tmpDraggedIndex + " This index = " + thisIndex);
+////                logger.info("Dragged Index Item = " + tmpDraggedIndex + " index item = " + tmpList.get(thisIndex));
+//
+//                int dragItemsStartIndex;
+//                int dragItemsEndIndex;
+//                int direction;
+//
+//
+//                if (cell.isEmpty()) {
+//                    dragItemsStartIndex = dragFromIndex.get();
+//                    dragItemsEndIndex = getListView().getItems().size();
+//                    direction = -1;
+//                } else {
+//                    if (cell.getIndex() < dragFromIndex.get()) {
+//                        dragItemsStartIndex = cell.getIndex();
+//                        dragItemsEndIndex = dragFromIndex.get() + 1;
+//                        direction = 1;
+//                    } else {
+//                        dragItemsStartIndex = dragFromIndex.get();
+//                        dragItemsEndIndex = cell.getIndex() + 1;
+//                        direction = -1;
+//                    }
+//                    logger.info("start " + dragItemsStartIndex + " stop " + dragItemsEndIndex);
+//                }
+//
+////                List<Item> rotatingItems = getListView().getItems().subList(dragItemsStartIndex, dragItemsEndIndex);
+////                List<Item> rotatingItemsCopy = new ArrayList<Item>(rotatingItems);
+////                Collections.rotate(rotatingItemsCopy, direction);
+////                rotatingItems.clear();
+////                rotatingItems.addAll(rotatingItemsCopy);
+////                dragFromIndex.set(-1);
+//
+//                success = true;
+//            }
+//
+//            event.setDropCompleted(success);
+//            event.consume();
+//        });
+//
+//        setOnDragDone(DragEvent::consume);
+    }
 
     @Override
     public void updateItem(Item item, boolean empty) {
@@ -52,5 +168,18 @@ public class PriorityCell extends JFXListCell<Item> {
             setText(null);
             setGraphic(gridPane_priority);
         }
+    }
+
+    public ObservableList<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(ObservableList<Item> items) {
+        this.items = items;
+    }
+
+    @Override
+    public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+
     }
 }
