ee36b36060ff093843d72104ec9ca0d056ec9589
Minh Nguyen <minh_pham@me.com>
2016-09-22 22:31:41 +0200

    production order ui elements

---
+++ src/main/java/de/ibsys/planningTool/controller/tab/ProductionOrderController.java
@@ -0,0 +1,13 @@
+package de.ibsys.planningTool.controller.tab;
+
+import javafx.stage.Stage;
+
+/**
+ * Created by minhnguyen on 22.09.16.
+ */
+public class ProductionOrderController extends BaseTabController {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
---
+++ src/main/java/de/ibsys/planningTool/controller/tab/productionOrderTab/MenBikeController.java
@@ -0,0 +1,7 @@
+package de.ibsys.planningTool.controller.tab.productionOrderTab;
+
+/**
+ * Created by minhnguyen on 22.09.16.
+ */
+public class MenBikeController {
+}
